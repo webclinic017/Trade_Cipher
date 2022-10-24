698b6be01c39f5b08d8df6a340c154bddcac68cd
Canion3D <80270614+canion3d@users.noreply.github.com>
2022-10-24 18:49:31 +0200

    Update main.py

--- main.py
+++ main.py
@@ -290,9 +290,8 @@ from pandas.core.common import flatten
 from tabulate import tabulate
 import yfinance as yf # https://pypi.org/project/yfinance/
 import ta as ta
-import ta.volatility
-import ta.trend
-import ta.momentum
+from ta import add_all_ta_features
+from ta.utils import dropna
 from ta.volatility import BollingerBands
 from ta.trend import MACD
 from ta.momentum import RSIIndicator
