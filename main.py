8b172ab360b80661c7e47541b364a8bb03a4a5f1
Canion3D <80270614+canion3d@users.noreply.github.com>
2022-10-24 17:16:15 +0200

    Update main.py

--- main.py
+++ main.py
@@ -273,37 +273,97 @@ if option == 'Twitter DB':
 
     symbol = st.sidebar.text_input("Ticker Symbol" , value = 'LTZBLD' , max_chars = 6)
 
-if option == 'RSI Dashboard':
-    st.subheader("Relative Strength Index Dashboard")
-
-#!/usr/bin/env python3
-
+import streamlit as st
+import pandas as pd
+import numpy as np
 import datetime
 import random
 import time
 from urllib.request import urlopen
-
 import matplotlib
 import matplotlib.dates as mdates
 import matplotlib.pyplot as plt
-import matplotlib.ticker as mticker
-import numpy as np
 import pandas_datareader.data as web
 import pylab
 from mplfinance.original_flavor import candlestick_ohlc
 from pandas.core.common import flatten
 from tabulate import tabulate
+import yfinance as yf # https://pypi.org/project/yfinance/
+from ta.volatility import BollingerBands
+from ta.trend import MACD
+from ta.momentum import RSIIndicator
 
-matplotlib.rcParams.update({'font.size': 9})
+###########
+# sidebar #
+###########
+option = st.sidebar.selectbox('Select one symbol', ( 'AAPL', 'MSFT',"SPY",'WMT'))
+import datetime
+today = datetime.date.today()
+before = today - datetime.timedelta(days=700)
+start_date = st.sidebar.date_input('Start date', before)
+end_date = st.sidebar.date_input('End date', today)
+if start_date < end_date:
+    st.sidebar.success('Start date: `%s`\n\nEnd date:`%s`' % (start_date, end_date))
+else:
+    st.sidebar.error('Error: End date must fall after start date.')
 
-# ---- HIDE STREAMLIT STYLE ----
-hide_st_style = """
-            <style>
-            #MainMenu {visibility: hidden;}
-            footer {visibility: hidden;}
-            header {visibility: hidden;}
-            </style>
-            """
-st.markdown(hide_st_style, unsafe_allow_html=True)
+##############
+# Stock data #
+##############
 
+# Download data
+df = yf.download(option,start= start_date,end= end_date, progress=False)
 
+# Bollinger Bands
+indicator_bb = BollingerBands(df['Close'])
+bb = df
+bb['bb_h'] = indicator_bb.bollinger_hband()
+bb['bb_l'] = indicator_bb.bollinger_lband()
+bb = bb[['Close','bb_h','bb_l']]
+
+# Moving Average Convergence Divergence
+macd = MACD(df['Close']).macd()
+
+# Resistence Strength Indicator
+rsi = RSIIndicator(df['Close']).rsi()
+
+###################
+# Set up main app #
+###################
+
+# Plot the prices and the bolinger bands
+st.write('Stock Bollinger Bands')
+st.line_chart(bb)
+
+progress_bar = st.progress(0)
+
+# Plot MACD
+st.write('Stock Moving Average Convergence Divergence (MACD)')
+st.area_chart(macd)
+
+# Plot RSI
+st.write('Stock RSI')
+st.line_chart(rsi)
+
+# Data of recent days
+st.write('Recent data')
+st.dataframe(df.tail(10))
+
+def to_excel(df):
+    output = df.to_excel
+    writer = pd.ExcelWriter(output, engine='xlsxwriter')
+    df.to_excel(writer, sheet_name='Sheet1')
+    writer.save()
+    processed_data = output.getvalue()
+    return processed_data
+
+def get_table_download_link(df):
+    """Generates a link allowing the data in a given panda dataframe to be downloaded
+    in:  dataframe
+    out: href string
+    """
+    val = to_excel(df)
+    b64 = base64.b64encode(val)  # val looks like b'...'
+    return f'<a href="data:application/octet-stream;base64,{b64.decode()}" download="download.xlsx">Download excel file</a>' # decode b'abc' => abc
+
+st.markdown(get_table_download_link(df), unsafe_allow_html=True)
