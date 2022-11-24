cf3cc6344e6bff6f69d6ea2f71d80ace7ef8ef1d
Canion3D <80270614+canion3d@users.noreply.github.com>
2022-11-24 10:57:53 +0100

    Update main.py

--- main.py
+++ main.py
@@ -49,6 +49,47 @@ cw.markets.get("KRAKEN:BTCUSD", orderbook=True)
 # Return market current orderbook liquidity
 cw.markets.get("KRAKEN:BTCUSD", liquidity=True)
 
+import cryptowatch as cw
+
+# Set your API Key
+cw.api_key = "2Q1K00N1JB9EHW5JNGGY"
+
+# Subscribe to resources (https://docs.cryptowat.ch/websocket-api/data-subscriptions#resources)
+cw.stream.subscriptions = ["markets:*:trades"]
+
+# What to do on each trade update
+def handle_trades_update(trade_update):
+    """
+        trade_update follows Cryptowatch protocol buffer format:
+        https://github.com/cryptowatch/proto/blob/master/public/markets/market.proto
+    """
+    market_msg = ">>> Market#{} Exchange#{} Pair#{}: {} New Trades".format(
+        trade_update.marketUpdate.market.marketId,
+        trade_update.marketUpdate.market.exchangeId,
+        trade_update.marketUpdate.market.currencyPairId,
+        len(trade_update.marketUpdate.tradesUpdate.trades),
+    )
+    print(market_msg)
+    for trade in trade_update.marketUpdate.tradesUpdate.trades:
+        trade_msg = "\tID:{} TIMESTAMP:{} TIMESTAMPNANO:{} PRICE:{} AMOUNT:{}".format(
+            trade.externalId,
+            trade.timestamp,
+            trade.timestampNano,
+            trade.priceStr,
+            trade.amountStr,
+        )
+        print(trade_msg)
+
+
+cw.stream.on_trades_update = handle_trades_update
+
+
+# Start receiving
+cw.stream.connect()
+
+# Call disconnect to close the stream connection
+# cw.stream.disconnect()
+
 
 option = st.sidebar.selectbox("Which Dashboard?", ('Main Page', 'Trade', 'Model Performance Analysis', 'TC Social', 'Charts','Twitter DB','RSI Dashboard'), 3)
 
