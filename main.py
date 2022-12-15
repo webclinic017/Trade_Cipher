035d997fa72d3c03aa9ec0b3058a24036ff5d9ed
Canion3D <80270614+canion3d@users.noreply.github.com>
2022-12-15 11:53:09 +0100

    N/A

--- main.py
+++ main.py
@@ -159,6 +159,36 @@ if option == 'Main Page':
         df_selected_sector.shape[1]) + ' columns.')
     st.dataframe(df_selected_sector)
 
+    # Import libraries
+    import streamlit as st
+    import pandas as pd
+    import matplotlib.pyplot as plt
+    import yfinance as yf
+
+    # Set title
+    st.title('Streaming Stock Market Charts')
+
+    # Get stock symbol from user
+    ticker = st.text_input('Enter a stock symbol:', '')
+
+    # Create empty figure
+    fig = plt.figure()
+
+    # Download stock data
+    tickerData = yf.Ticker(ticker)
+
+
+    # Create streaming plot
+    def create_plot():
+        df = tickerData.history(period="3mo")
+        plt.plot(df['Close'])
+        st.pyplot()
+
+
+    # Run streaming plot
+    if st.checkbox('Show streaming plot'):
+        create_plot()
+
 
     # Download S&P500 data
     # https://discuss.streamlit.io/t/how-to-download-file-in-streamlit/1806
