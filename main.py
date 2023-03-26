d0d19d1dfff8834cfa13ed933797efbb7e9b0b17
Canion3D <80270614+canion3d@users.noreply.github.com>
2023-03-26 21:02:07 +0200

    Update main.py

--- main.py
+++ main.py
@@ -95,32 +95,35 @@ if option == 'Main Page':
 
     # Set title
 st.title('Business News')
-    
-   
+
 from requests import Request, Session
 from requests.exceptions import ConnectionError, Timeout, TooManyRedirects
 import json
 
-url = 'https://sandbox-api.coinmarketcap.com/v1/cryptocurrency/listings/latest'
-parameters = {
-  'start':'1',
-  'limit':'5000',
-  'convert':'USD'
-}
-headers = {
-  'Accepts': 'application/json',
-  'X-CMC_PRO_API_KEY': '1fdbee66-c1d9-45ee-9942-90f3270866f2',
-}
+from newsapi import NewsApiClient
 
-session = Session()
-session.headers.update(headers)
+# Init
+newsapi = NewsApiClient(api_key='e05f54f819fb43b4b67385072ad1db10')
 
-try:
-  response = session.get(url, params=parameters)
-  data = json.loads(response.text)
-  print(data)
-except (ConnectionError, Timeout, TooManyRedirects) as e:
-  print(e)
+# /v2/top-headlines
+top_headlines = newsapi.get_top_headlines(q='bitcoin',
+                                          sources='bbc-news,the-verge',
+                                          category='business',
+                                          language='en',
+                                          country='us')
+
+# /v2/everything
+all_articles = newsapi.get_everything(q='bitcoin',
+                                      sources='bbc-news,the-verge',
+                                      domains='bbc.co.uk,techcrunch.com',
+                                      from_param='2017-12-01',
+                                      to='2017-12-12',
+                                      language='en',
+                                      sort_by='relevancy',
+                                      page=2)
+
+# /v2/top-headlines/sources
+sources = newsapi.get_sources()
 
 if option == 'TC Social':
     st.subheader("Trade Cipher Social Platform")
