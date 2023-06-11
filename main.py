edbe59832bf66407b26256ab4e09cfe83b60f08a
Canion3D <80270614+canion3d@users.noreply.github.com>
2023-06-11 21:01:24 +0200

    Update main.py

--- main.py
+++ main.py
@@ -333,3 +333,16 @@ if option == 'Twitter DB':
 st.markdown("""
 <a href="https://click.linksynergy.com/fs-bin/click?id=8WC05bHq4DI&offerid=1207190.332&subid=0&type=4"><IMG border="0"   alt="Newegg" src="https://ad.linksynergy.com/fs-bin/show?id=8WC05bHq4DI&bids=1207190.332&subid=0&type=4&gridnum=1"></a>
 """, unsafe_allow_html=True)
+
+import os
+
+# List available page files
+page_files = os.listdir("Pages")
+page_files = [file for file in page_files if file.endswith(".py")]
+
+# Add a menu item to select a page
+selected_page = st.sidebar.selectbox("Select a page", page_files)
+
+# Execute the selected page
+if selected_page:
+    exec(open(f"Pages/{selected_page}").read())
