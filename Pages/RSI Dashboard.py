5e9c20fbfde1409dddd3ea5ee16220125578c811
RBI <80270614+rbi@users.noreply.github.com>
2023-07-05 21:01:19 +0200

    na

--- Pages/RSI Dashboard.py
+++ Pages/RSI Dashboard.py
@@ -29,7 +29,7 @@ with col3:
 
 st.markdown("<h2 style='text-align: center; color: white;'>Select a dashboard to get started: </h2>" , unsafe_allow_html = True)
 
-st.markdown("<h1 style='text-align: center;'>BYOB is here with Trade Cipher! Bring Your Own Broker is the future!</h1>", unsafe_allow_html=True)
+st.markdown("<h1 style='text-align: center;'>To get started, enter a trading symbol in the side bar under Stock Selection!</h1>", unsafe_allow_html=True)
 
 # Add divider line
 st.markdown("---")
