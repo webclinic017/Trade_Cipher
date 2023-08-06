4cdaba12aeb85b4605865afea9e73e983a975b1a
RBI <80270614+rbi@users.noreply.github.com>
2023-08-06 15:29:15 +0200

    N/A

--- Pages/Crypto.py
+++ Pages/Crypto.py
@@ -1,6 +1,19 @@
 import requests
 import streamlit
 
+url = "https://seeking-alpha-finance.p.rapidapi.com/v1/search/advanced"
+
+querystring = {"query":"bill","search_advanced_type":"people"}
+
+headers = {
+	"X-RapidAPI-Key": "d29531ae60msh09b292159b1d570p1db262jsnd2039bec5d98",
+	"X-RapidAPI-Host": "seeking-alpha-finance.p.rapidapi.com"
+}
+
+response = requests.get(url, headers=headers, params=querystring)
+
+print(response.json())
+
 url = "https://coinranking1.p.rapidapi.com/coins"
 
 querystring = {"referenceCurrencyUuid":"yhjMzLPhuIDl","timePeriod":"24h","tiers[0]":"1","orderBy":"marketCap","orderDirection":"desc","limit":"50","offset":"0"}
