728ebdc3d3b8dd2a42bed1ddd753ca16d3fb5d56
RBI <80270614+rbi@users.noreply.github.com>
2023-08-06 16:00:23 +0200

    N/A

--- Pages/News.py
+++ Pages/News.py
@@ -1,77 +1,70 @@
 import requests
 import streamlit as st
 
-import streamlit as st
-import requests
+# Create the tabs
+tab1, tab2 = st.tabs(["Current Business News", "Wall Street Journal Articles"])
 
-tab1, tab2, = st.tabs(["Current Business News", "Wall Street Journal Articles"])
-
-
-with tab1:
+if tab1:  # Only fetch and display data if tab1 is selected
     st.header("Current Business News")
 
-st.header("Business News (Last 24 hrs.)")
+    # Set the API endpoint and parameters
+    url = "https://newsapi.org/v2/top-headlines?country=us&category=business"
+    params = {"country": "us", "apiKey": "e05f54f819fb43b4b67385072ad1db10"}
 
-# set the API endpoint and parameters
-url = "https://newsapi.org/v2/top-headlines?country=us&category=business"
-params = {"country": "us", "apiKey": "e05f54f819fb43b4b67385072ad1db10"}
+    # Make the API request and retrieve the data
+    response = requests.get(url, params=params)
+    data = response.json()
 
-# make the API request and retrieve the data
-response = requests.get(url, params=params)
-data = response.json()
+    # Display the data in the Streamlit app with formatting
+    st.markdown(
+        """
+        <div style='text-align: center;'>
+            <h1>Business News</h1>
+        </div>
+        """,
+        unsafe_allow_html=True
+    )
+    st.write("Here are the top headlines in business:")
+    for article in data["articles"]:
+        st.write("## " + article["title"])
+        st.write(article["description"])
+        st.write(f"Source: {article['source']['name']}  Published: {article['publishedAt']}")
+        st.write("---")
 
-# display the data in the Streamlit app with formatting
+        # Set the font family and size using HTML tags
+        html = f"<div style='font-family: Arial; font-size: 12pt'></div>"
+        # Write the HTML to the Streamlit app
+        st.write(html, unsafe_allow_html=True)
 
-st.markdown(
-    """
-    <div style='text-align: center;'>
-        <h1>Business News</h1>
-    </div>
-    """,
-    unsafe_allow_html=True
-)
-st.write("Here are the top headlines in business:")
-for article in data["articles"]:
-    st.write("## " + article["title"])
-    st.write(article["description"])
-    st.write(f"Source: {article['source']['name']}  Published: {article['publishedAt']}")
-    st.write("---")
-
-# Set the font family and size using HTML tags
-html = f"<div style='font-family: Arial; font-size: 12pt </div>"
-# Write the HTML to the Streamlit app
-st.write(html, unsafe_allow_html=True)
-
-with tab2:
+if tab2:  # Only fetch and display data if tab2 is selected
     st.header("Wall Street Journal Articles")
 
-# set the API endpoint and parameters
-url = "https://newsapi.org/v2/everything?domains=wsj.com"
-params = {"apiKey": "e05f54f819fb43b4b67385072ad1db10"}
+    # Set the API endpoint and parameters
+    url = "https://newsapi.org/v2/everything?domains=wsj.com"
+    params = {"apiKey": "e05f54f819fb43b4b67385072ad1db10"}
 
-# make the API request and retrieve the data
-response = requests.get(url, params=params)
-data = response.json()
+    # Make the API request and retrieve the data
+    response = requests.get(url, params=params)
+    data = response.json()
 
-# display the data in the Streamlit app with formatting
+    # Display the data in the Streamlit app with formatting
+    st.markdown(
+        """
+        <div style='text-align: center;'>
+            <h1>WSJ Articles</h1>
+        </div>
+        """,
+        unsafe_allow_html=True
+    )
+    st.write("Here are the top articles from WSJ:")
+    for article in data["articles"]:
+        st.write("## " + article["title"])
+        st.write(article["description"])
+        st.write(f"Source: {article['source']['name']}  Published: {article['publishedAt']}")
+        st.write("---")
+        st.write('url')
 
-st.markdown(
-    """
-    <div style='text-align: center;'>
-        <h1>WSJ Articles</h1>
-    </div>
-    """,
-    unsafe_allow_html=True
-)
-st.write("Here are the top articles from WSJ:")
-for article in data["article"]:
-    st.write("## " + article["title"])
-    st.write(article["description"])
-    st.write(f"Source: {article['source']['name']}  Published: {article['publishedAt']}")
-    st.write("---")
-    st.write('url')
-
-# Set the font family and size using HTML tags
-html = f"<div style='font-family: Arial; font-size: 12pt </div>"
-# Write the HTML to the Streamlit app
-st.write(html, unsafe_allow_html=True)
+        # Set the font family and size using HTML tags
+        html = f"<div style='font-family: Arial; font-size: 12pt'></div>"
+        # Write the HTML to the Streamlit app
+        st.write(html, unsafe_allow_html=True)
