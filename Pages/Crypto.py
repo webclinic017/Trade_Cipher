70fc0bba82c227f41aabb967c0f3550e383eca65
RBI <80270614+rbi@users.noreply.github.com>
2023-08-07 10:59:05 +0200

    N/A

--- Pages/Crypto.py
+++ Pages/Crypto.py
@@ -1,18 +1,53 @@
+import streamlit as st
+import time
+
+progress_text = st.empty()
+my_bar = st.progress(0)
+
+progress_text.markdown("**Just a moment while we prepare the environment!**")
+
+for percent_complete in range(100):
+    time.sleep(0.1)
+    my_bar.progress(percent_complete + 1)
+
+progress_text.markdown("**The environment is ready!**")
+
 import requests
 import streamlit
 
-url = "https://seeking-alpha-finance.p.rapidapi.com/v1/search/advanced"
+import streamlit as st
+import requests
 
-querystring = {"query":"bill","search_advanced_type":"people"}
+def fetch_api_data():
+    url = "https://seeking-alpha-finance.p.rapidapi.com/v1/search/advanced"
+    querystring = {"query": "bill", "search_advanced_type": "people"}
+    headers = {
+        "X-RapidAPI-Key": "d29531ae60msh09b292159b1d570p1db262jsnd2039bec5d98",
+        "X-RapidAPI-Host": "seeking-alpha-finance.p.rapidapi.com"
+    }
 
-headers = {
-	"X-RapidAPI-Key": "d29531ae60msh09b292159b1d570p1db262jsnd2039bec5d98",
-	"X-RapidAPI-Host": "seeking-alpha-finance.p.rapidapi.com"
-}
+    response = requests.get(url, headers=headers, params=querystring)
+    return response.json()
 
-response = requests.get(url, headers=headers, params=querystring)
+def main():
+    st.title("Seeking Alpha API Streamlit App")
+    st.subheader("Search Results for 'bill' in People")
 
-print(response.json())
+    # Fetch API data
+    data = fetch_api_data()
+
+    # Display data on the front-end
+    if "data" in data:
+        for result in data["data"]:
+            st.write(f"Name: {result['attributes']['name']}")
+            st.write(f"Role: {result['attributes']['role']}")
+            st.write("------")
+    else:
+        st.write("No results found.")
+
+if __name__ == "__main__":
+    main()
+
 
 url = "https://coinranking1.p.rapidapi.com/coins"
 
