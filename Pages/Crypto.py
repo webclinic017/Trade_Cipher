49cf92411d0285e28e33895d2b04aaf4690b2b49
RBI <80270614+rbi@users.noreply.github.com>
2023-08-07 12:00:54 +0200

    N/A

--- Pages/Crypto.py
+++ Pages/Crypto.py
@@ -34,34 +34,5 @@ def main():
     else:
         st.write("Failed to fetch data from the API. Please check your API key and connection.")
 
-url = "https://coinranking1.p.rapidapi.com/coins"
-
-querystring = {"referenceCurrencyUuid":"yhjMzLPhuIDl","timePeriod":"24h","tiers[0]":"1","orderBy":"marketCap","orderDirection":"desc","limit":"50","offset":"0"}
-
-headers = {
-	"X-RapidAPI-Key": "d29531ae60msh09b292159b1d570p1db262jsnd2039bec5d98",
-	"X-RapidAPI-Host": "coinranking1.p.rapidapi.com"
-}
-
-response = requests.get(url, headers=headers, params=querystring)
-
-print(response.json())
-
-import http.client
-
-conn = http.client.HTTPSConnection("alpha-vantage.p.rapidapi.com")
-
-headers = {
-    'X-RapidAPI-Key': "62b008b623mshaf55ca1e208d945p1256acjsn8ca0e82661f6",
-    'X-RapidAPI-Host': "alpha-vantage.p.rapidapi.com"
-}
-
-conn.request("GET", "/query?interval=5min&function=TIME_SERIES_INTRADAY&symbol=MSFT&datatype=json&output_size=compact", headers=headers)
-
-res = conn.getresponse()
-data = res.read()
-
-print(data.decode("utf-8"))
-
 if __name__ == "__main__":
     main()
