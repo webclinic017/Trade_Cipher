8260699e0da9184e19940ae7ef234a32db0f7300
Canion3D <80270614+canion3d@users.noreply.github.com>
2023-08-13 10:25:49 +0200

    Update Crypto.py

--- Pages/Crypto.py
+++ Pages/Crypto.py
@@ -1,6 +1,24 @@
-import requests
 import streamlit as st
+import time
+import requests
 import pandas as pd
+import json
+
+import http.client
+
+conn = http.client.HTTPSConnection("seeking-alpha-finance.p.rapidapi.com")
+
+headers = {
+    'X-RapidAPI-Key': "62b008b623mshaf55ca1e208d945p1256acjsn8ca0e82661f6",
+    'X-RapidAPI-Host': "seeking-alpha-finance.p.rapidapi.com"
+}
+
+conn.request("GET", "/v1/screeners/tickers?screener_id=96793299", headers=headers)
+
+res = conn.getresponse()
+data = res.read()
+
+print(data.decode("utf-8"))
 
 def get_api_data():
     url = "https://seeking-alpha-finance.p.rapidapi.com/v1/screeners/tickers"
@@ -21,11 +39,19 @@ def main():
     data = get_api_data()
 
     if data:
+        # Convert data to a JSON string and prettify it
+        json_string = json.dumps(data, indent=4)
+
+        # Display the JSON response using st.code() to maintain formatting
+        st.write("API Response (Raw JSON):")
+        st.code(json_string, language="json")
+
         # Convert data to a pandas DataFrame
         df = pd.DataFrame(data)
 
-        # Set a unique index for the DataFrame
-        df.set_index("Company Name", inplace=True)  # Replace with an actual unique column name
+        # Display the DataFrame with st.dataframe()
+        st.write("API Response (Parsed Data):")
+        st.dataframe(df)
 
         # Display the DataFrame as an HTML table with custom styling
         st.write("API Response (Formatted Table):")
