ada558aa4aaf59764bf6c9264dde2b318fe191d5
Canion3D <80270614+canion3d@users.noreply.github.com>
2023-08-24 10:54:45 +0200

    Update Dividends.py

--- Pages/Dividends.py
+++ Pages/Dividends.py
@@ -33,3 +33,32 @@ st.markdown("<h1 style='text-align: center;'>BYOB is here with Trade Cipher! Bri
 
 # Add divider line
 st.markdown("---")
+
+try:
+    # For Python 3.0 and later
+    from urllib.request import urlopen
+except ImportError:
+    # Fall back to Python 2's urllib2
+    from urllib2 import urlopen
+
+import certifi
+import json
+
+def get_jsonparsed_data(url):
+    """
+    Receive the content of ``url``, parse it as JSON and return the object.
+
+    Parameters
+    ----------
+    url : str
+
+    Returns
+    -------
+    dict
+    """
+    response = urlopen(url, cafile=certifi.where())
+    data = response.read().decode("utf-8")
+    return json.loads(data)
+
+url = ("https://financialmodelingprep.com/api/v3/stock_dividend_calendar?from=2020-06-01&to=2020-09-10&apikey=7871663557cbf06ab075c5ba73a7098f")
+print(get_jsonparsed_data(url))
